6df11c43789d35dce64f1c6a57c50d9de0efc54b
SigurD <SigurD@SIGURD->
2017-12-06 20:59:58 +0300

    First employment commit

---
+++ EmploymentTestLibrary/EmploymentTestLibrary/TriangleAreaCalculator.cs
@@ -0,0 +1,43 @@
+﻿//       |\
+//       | \
+//       |  \
+//       |   \
+//     A |    \ C
+//       |     \
+//       |      \
+//       |_ _ _ _\
+//          B
+//
+using System;
+
+namespace EmploymentLibrary
+{
+    /// <summary>
+    /// Self-explanatory class name
+    /// </summary>
+    public class TriangleAreaCalculator
+    {
+        /// <summary>
+        /// Self-explanatory method name
+        /// </summary>
+        /// <param name="a">side a</param>
+        /// <param name="b">side b</param>
+        /// <param name="c"> side c</param>
+        /// <returns>Area of given triangle</returns>
+        public double GetTriangleArea(double a, double b, double c)
+        {
+            if (a < 0d || b < 0d || c < 0d)
+            {
+                throw new ArgumentOutOfRangeException("One of arguments has negative length value");
+            }
+
+            if (Math.Pow(c, 2) != Math.Pow(a, 2) + Math.Pow(b, 2))
+            {
+                throw new ArgumentException($"Triangle is not right. Variable c doesn't corresponds the rule: c^2 = a^2 + b^2; a={a}, b={a}, c={c}");
+            }
+
+            return (a * b) / 2;
+        }
+
+    }
+}
---
+++ EmploymentTestLibrary/UnitTestProject1/TriangleAreaCalculatorTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EmploymentLibrary;
+
+namespace TriangleCalculator.Tests
+{
+    [TestClass]
+    public class TriangleAreaCalculatorTests
+    {
+        [TestMethod]
+        public void GetTriangleArea_3and4and5_6returned()
+        {
+            double a = 3d;
+            double b = 4d;
+            double c = 5d;
+            double expected = 6d;
+
+            var triangleCalc = new TriangleAreaCalculator();
+            var actual = triangleCalc.GetTriangleArea(a, b, c);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), AllowDerivedTypes = false)]
+        public void GetTriangleArea_ArgumentThrown()
+        {
+            double a = 3d;
+            double b = 5d;
+            //
+            double c = 42d;
+
+            var triangleCalc = new TriangleAreaCalculator();
+            var actual = triangleCalc.GetTriangleArea(a, b, c);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException), AllowDerivedTypes = false)]
+        public void GetTriangleArea_ArgumentOutOfRangeThrown()
+        {
+            double a = -3d;
+            double b = 5d;
+            double c = Math.Pow(Math.Pow(a, 2) + Math.Pow(b, 2), 0.5d);
+
+            var triangleCalc = new TriangleAreaCalculator();
+            var actual = triangleCalc.GetTriangleArea(a, b, c);
+        }
+    }
+}
